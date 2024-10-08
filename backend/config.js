d3290624dd5cff349b8f5868bc582ad206fe1765
navya1220 <navyakalla1111@gmail.com>
2024-10-08 16:53:45 +0530

    vercel.json

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,3 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://gnaneswarkalla1111:root@bookstore.3syba.mongodb.net/?retryWrites=true&w=majority&appName=bookstore'
+export const mongoDBURL = 'mongodb+srv://gnaneswarkalla1111:root@bookstore.3syba.mongodb.net/?retryWrites=true&w=majority&appName=bookstore';
