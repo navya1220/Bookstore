bccf33c822496c0814873b955502964c13412931
navya1220 <navyakalla1111@gmail.com>
2024-11-20 12:44:44 +0530

    updated

--- backend/controllers/bookController.js
+++ backend/controllers/bookController.js
@@ -40,3 +40,83 @@ export const getAllBooks = async (request, response) => {
       response.status(500).send({ message: error.message });
     }
   };
+
+  export const deleteBook = async (request, response) => {
+    try {
+      const { id } = request.params;
+  
+      const book = await Book.findByIdAndDelete(id);
+  
+      if (!book) {
+        return response.status(404).json({
+          message: 'Book not found',
+        });
+      }
+      
+      return response.status(200).json({
+        message: 'Book deleted successfully',
+      });
+    } catch (error) {
+      console.error(error.message);
+      response.status(500).json({
+        message: 'An error occurred while deleting the book',
+      });
+    }
+  };
+
+
+export const editBook = async (request, response) => {
+    try {
+      const { id } = request.params; 
+      const { title, author, publishYear } = request.body; 
+  
+      if (!title || !author || !publishYear) {
+        return response.status(400).json({
+          message: 'Please provide title, author, and publishYear to update the book.',
+        });
+      }
+  
+      const updatedBook = await Book.findByIdAndUpdate(
+        id,
+        { title, author, publishYear },
+        { new: true, runValidators: true } 
+    );
+    if (!updatedBook) {
+      return response.status(404).json({
+        message: 'Book not found',
+      });
+    }
+    return response.status(200).json({
+      message: 'Book updated successfully',
+      data: updatedBook,
+    });
+  } catch (error) {
+    console.error(error.message);
+    response.status(500).json({
+      message: 'An error occurred while updating the book',
+    });
+  }
+};
+
+
+export const getBookById = async (request, response) => {
+  try {
+    const { id } = request.params; 
+
+    const book = await Book.findById(id);
+    if (!book) {
+      return response.status(404).json({
+        message: 'Book not found',
+      });
+    }
+    return response.status(200).json({
+      message: 'Book fetched successfully',
+      data: book,
+    });
+  } catch (error) {
+    console.error(error.message);
+    response.status(500).json({
+      message: 'An error occurred while fetching the book details',
+    });
+  }
+};
--- backend/routes/booksRoute.js
+++ backend/routes/booksRoute.js
@@ -1,10 +1,16 @@
 import express from 'express';
-import { createBook, getAllBooks } from '../controllers/bookController.js';
+import { createBook, getAllBooks, deleteBook, editBook, getBookById } from '../controllers/bookController.js';
 
 const router = express.Router();
 
 router.post('/', createBook);
 
 router.get('/', getAllBooks);
+
+router.delete('/:id', deleteBook);
+
+router.put('/:id', editBook);
+
+router.get('/:id', getBookById);
 
 export default router;
